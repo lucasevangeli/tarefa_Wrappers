29eeea62f18d64d5d3343446def7460a3d0c116f
Lucas Evan <lucasevan478@gmail.com>
2024-07-05 11:45:43 -0300

    Tarefa Wrappers

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int cpf = Integer.valueOf(050432);
+
+        long nome = Long.valueOf(cpf);
+        
+        System.out.println(nome);
+        
+    }
+    
+}
